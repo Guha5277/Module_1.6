ac58465d6f99bbe0d66c05c40e7fc8a8a935928e
Алексей <guha5277@yandex.ru>
2020-10-21 11:35:40 +0700

    Добавление точки входа программы к шаблону "Прототип"

---
+++ java/guhar4k/patterns/prototype/SecurityCheckpointRunner.java
@@ -0,0 +1,23 @@
+package guhar4k.patterns.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityCheckpointRunner {
+    public static void main(String[] args) {
+        List<SecurityCheckpointRecord> checkpointRecords = new ArrayList<>();
+        SecurityCheckpointRecord record = new SecurityCheckpointRecord("John");
+        SecurityCheckpointRecordFactory factory = new SecurityCheckpointRecordFactory(record);
+        checkpointRecords.add(factory.copy());
+
+        record.registerNewPerson("Elizabeth");
+        checkpointRecords.add(factory.copy());
+
+        record.registerNewPerson("David");
+        checkpointRecords.add(factory.copy());
+
+        for (SecurityCheckpointRecord rec : checkpointRecords) {
+            System.out.println(rec);
+        }
+    }
+}
